8519dac1b4ef5de12cde816adabc7452d52a1d73
Xbey25 <luisdgb2506@gmail.com>
2022-11-04 11:06:55 +0100

    WebAPI working for req 1

--- C#/SEP3/Shared/domain/Product.cs
+++ C#/SEP3/Shared/domain/Product.cs
@@ -6,7 +6,7 @@ public class Product
     public string name { get; set; } 
     public string description { get; set; }
     public double price { get; set; }
-    public Category category;
+   // public Category category;
 
   
 }
