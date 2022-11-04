1447a566a1dd7157e12bcfde89745a1a268135fa
Xbey25 <luisdgb2506@gmail.com>
2022-11-04 11:18:54 +0100

    Updates

--- C#/SEP3/BlazorClient/Program.cs
+++ C#/SEP3/BlazorClient/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorClient;
+using HttpClients.ClientInterfaces;
+using HttpClients.Implementations;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { 
     BaseAddress = new Uri("https://localhost:5238") 
 });
+builder.Services.AddScoped<IProductService, ProductHttpClient>();
 
 await builder.Build().RunAsync();
 
--- C#/SEP3/HttpClients/ClientInterfaces/IProductService.cs
+++ C#/SEP3/HttpClients/ClientInterfaces/IProductService.cs
@@ -5,5 +5,5 @@ namespace HttpClients.ClientInterfaces;
 public interface IProductService
 {
     //Task<Product> Create(ProductCreationDto dto);
-    Task<IEnumerable<Product>> GetProducts(string? nameContains = null); //namecontains may change depending on the webapi
+    Task<IEnumerable<Product>> GetProducts(string? nameContains = null);
 }
