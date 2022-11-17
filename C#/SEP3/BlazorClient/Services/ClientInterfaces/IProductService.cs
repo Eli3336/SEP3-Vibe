ae2129e861f4136ae8e128a71a4c8b02a30039ea
Eli3336 <mihai_elisabeta_3@yahoo.com>
2022-11-17 16:55:23 +0100

    delete

--- C#/SEP3/BlazorClient/Services/ClientInterfaces/IProductService.cs
+++
@@ -1,9 +0,0 @@
-using Shared;
-
-namespace HttpClients.ClientInterfaces;
-
-public interface IProductService
-{
-    //Task<Product> Create(ProductCreationDto dto);
-    Task<IEnumerable<Product>> GetProducts(string? nameContains = null);
-}
