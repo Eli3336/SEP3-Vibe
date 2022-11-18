4b39a35e90f64ff951f61a76eafe8ffdbf96f6cf
Eli3336 <mihai_elisabeta_3@yahoo.com>
2022-11-18 08:47:11 +0100

    auth error

--- C#/SEP3/Shared/Auth/AuthorizationPolicies.cs
+++ C#/SEP3/Shared/Auth/AuthorizationPolicies.cs
@@ -1,18 +1,17 @@
 using Microsoft.Extensions.DependencyInjection;
-
 namespace Shared.Auth;
 
 public static class AuthorizationPolicies
 {
     public static void AddPolicies(IServiceCollection services)
     {
-        //services.AddAuthorizationCore(options =>
-        //{
-           // options.AddPolicy("MustBeLoggedIn", a =>
-             //   a.RequireAuthenticatedCustomer());
+        services.AddAuthorizationCore(options =>
+        {
+            options.AddPolicy("MustBeLoggedIn", a =>
+                a.RequireAuthenticatedCustomer());
     
             
                 
-      //  });
+        });
     }   
 }
--- C#/SEP3/ShopAPI/Program.cs
+++ C#/SEP3/ShopAPI/Program.cs
@@ -66,8 +66,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
