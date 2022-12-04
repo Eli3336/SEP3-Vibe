dd241373077feefda08c30d4427866ff7ebe8eb2
domignat <92146634+domignat@users.noreply.github.com>
2022-12-04 20:34:03 +0100

    Java working

--- Java/Supplier/src/main/java/via/sep3/supplier/data/repository/IProductRep.java
+++ Java/Supplier/src/main/java/via/sep3/supplier/data/repository/IProductRep.java
@@ -1,10 +1,12 @@
 package via.sep3.supplier.data.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import via.sep3.supplier.domain.Product;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -13,10 +15,13 @@ public interface IProductRep extends JpaRepository<Product, Long>
     @Query(value = "SELECT * FROM products  WHERE id = :product_id",nativeQuery = true)
     List<Product> getProductById(Long product_id);
 
+
+    @Transactional
+    @Modifying
     @Query(value = "UPDATE products  SET product_name= :product_name ," +
             " product_description= :product_description, price= :price " +
             "WHERE  id = :product_id",nativeQuery = true)
-    Product editProductById(Long product_id, String product_name, String product_description, double price);
+    void editProductById(Long product_id, String product_name, String product_description, double price);
 
 
 
